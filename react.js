8265eefdd965b99bad160053ac9824b3757c4fe1
Guga Villar <grsv.21@gmail.com>
2022-12-01 08:29:03 -0300

    fix: no-implicit-coercion, no-extra-boolean-cast and no-unused-vars eslint

--- react.js
+++ react.js
@@ -5,51 +5,63 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'standard',
-    'plugin:prettier/recommended',
+    "next",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "standard",
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    'jsx-a11y',
-    '@typescript-eslint'
+  plugins: ["react", "jsx-a11y", "@typescript-eslint"],
+  ignorePatterns: [
+    "package.json",
+    "package-lock.json",
+    "yarn.lock",
+    "index.html",
+    "**/**/*.html",
+    "README.md",
+    ".eslintrc.js",
   ],
-  ignorePatterns: ["package.json", "package-lock.json", "yarn.lock", "index.html", "**/**/*.html"],
   rules: {
-    'prettier/prettier': ['error', {
-      'printWidth': 120,
-      'tabWidth': 2,
-      'singleQuote': true,
-      'trailingComma': 'none',
-      'arrowParens': 'always',
-      'semi': false,
-      'singleAttributePerLine': true,
-      'endOfLine': 'auto'
-    }],
-    'camelcase': 'off',
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    'import/order': [
-      'error',
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { ignoreRestSiblings: true },
+    ],
+    "prettier/prettier": [
+      "error",
       {
-        'groups': [
-          'external',
-          'builtin',
-          'internal',
-          [
-            'parent',
-            'sibling'
-          ],
-          ['index', 'object', 'type']
+        printWidth: 120,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: "none",
+        arrowParens: "always",
+        semi: false,
+        singleAttributePerLine: true,
+        endOfLine: "auto",
+      },
+    ],
+    "no-extra-boolean-cast": "off",
+    camelcase: "off",
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "array-callback-return": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["external", "builtin"],
+          "internal",
+          ["parent", "sibling"],
+          ["index", "object", "type"],
         ],
         'pathGroups': [
           {
@@ -63,41 +75,37 @@ module.exports = {
             'position': 'before'
           }
         ],
-        'pathGroupsExcludedImportTypes': [
-          'react',
-          'react-dom'
-        ],
-        'newlines-between': 'always',
-        'alphabetize': {
-          'order': 'asc',
-          'caseInsensitive': true
-        }
-      }
-    ],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': [
-      'warn',
-      {
-        elements: ['img'],
-        img: ['Image'],
+        pathGroupsExcludedImportTypes: [],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
       },
     ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
+    "jsx-a11y/alt-text": [
+      "warn",
+      {
+        elements: ["img"],
+        img: ["Image"],
+      },
+    ],
+    "no-implicit-coercion": "warn",
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
-    'import/resolver': {
-      'typescript': {}
+    "import/resolver": {
+      typescript: {},
     },
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
-    }
-  }
-}
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    },
+  },
+};
