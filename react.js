73f56512ca5e5e71fb99117b181b324e239171eb
Guga Villar <grsv.21@gmail.com>
2022-12-20 05:59:57 -0300

    feat: add sonar lint

--- react.js
+++ react.js
@@ -2,93 +2,98 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
   extends: [
-    "next",
-    "standard",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:prettier/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react", "jsx-a11y", "prettier", "@typescript-eslint"],
-  ignorePatterns: [
-    "package.json",
-    "yarn.lock",
-  ],
+  plugins: ['react', 'jsx-a11y', 'prettier', '@typescript-eslint'],
+  ignorePatterns: ['yarn.lock', 'package.json'],
   rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { ignoreRestSiblings: true },
-    ],
-    "prettier/prettier": [
-      "error",
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'prettier/prettier': [
+      'error',
       {
         printWidth: 120,
         tabWidth: 2,
         singleQuote: true,
-        trailingComma: "none",
-        arrowParens: "always",
+        jsxSingleQuote: true,
+        trailingComma: 'none',
+        arrowParens: 'always',
         semi: false,
         singleAttributePerLine: true,
-        endOfLine: "auto",
-      },
+        endOfLine: 'auto'
+      }
     ],
-    "no-extra-boolean-cast": "off",
-    camelcase: "off",
-    "no-unused-vars": "off",
-    "no-undef": "off",
-    "array-callback-return": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "import/order": [
-      "error",
+    'no-extra-boolean-cast': 'off',
+    camelcase: 'off',
+    'sonarjs/no-duplicate-string': 'off',
+    'sonarjs/prefer-single-boolean-return': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'array-callback-return': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'import/order': [
+      'error',
       {
-        groups: [
-          ["external", "builtin"],
-          "internal",
-          ["parent", "sibling"],
-          ["index", "object", "type"],
+        groups: [['external', 'builtin'], 'internal', ['parent', 'sibling'], ['index', 'object', 'type']],
+        pathGroups: [
+          {
+            pattern: '@chakra-ui/react',
+            group: 'internal',
+            position: 'before'
+          },
+          {
+            pattern: '@*/components',
+            group: 'internal',
+            position: 'before'
+          }
         ],
         pathGroupsExcludedImportTypes: [],
-        "newlines-between": "always",
+        'newlines-between': 'always',
         alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-      },
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
     ],
-    "jsx-a11y/alt-text": [
-      "warn",
+    'jsx-a11y/alt-text': [
+      'warn',
       {
-        elements: ["img"],
-        img: ["Image"],
-      },
+        elements: ['img'],
+        img: ['Image']
+      }
     ],
-    "no-implicit-coercion": "warn",
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
+    'no-implicit-coercion': ['warn', { allow: ['!!'] }],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn'
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect'
     },
-    "import/resolver": {
-      typescript: {},
+    'import/resolver': {
+      typescript: {}
     },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
-    },
-  },
-};
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts']
+    }
+  }
+}
